d40de563f8da2dbc0071c35e80bd66a2b235ac57
robinabdullah <Abdullah.Robin@nuarca.com>
2022-09-21 03:28:57 +0600

    Add project files.

---
+++ Competitive_Programming/Math/Palindrome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competitive_Programming.Math
+{
+    public class Palindrome
+    {
+        public static bool IsPalindrome(int x)
+        {
+            string toCheck = x.ToString();
+            int count = toCheck.Count();
+            for (int i = 0; i < count; i++)
+            {
+                if (toCheck[i] == toCheck[count - 1 - i])
+                    continue;
+                else
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Competitive_Programming/Program.cs
@@ -0,0 +1,14 @@
+﻿using Competitive_Programming.Math;
+
+namespace Competitive_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Palindrome.IsPalindrome(5555));
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
